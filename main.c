d54fa6dd7aab40682b64e9bd21ffbc297d783db7
Ryan Michael Wurtz <rwurtz@purdue.edu>
2024-08-29 15:47:53 -0400

    4

--- main.c
+++ main.c
@@ -6,16 +6,19 @@
 int main(int argc, char * * argv) {
     int total;
     scanf("%d",&total);
+    //*declaring variables for iterating/keeping track of remaining total
     int i;
     int j;
     int k;
     int qrem;
     int drem;
+    //*iterating through, from high-low, the coin combinations
     for (i=total/25;i>=0;i--) {
         qrem = total - 25*i;
         for (j=qrem/10;j>=0;j--) {
             drem = qrem - 10*j;
             for (k=drem/5;k>=0;k--) {
+                //*printing combo every iteration
                 printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n",i,j,k,drem - 5*k);
             }
         }
